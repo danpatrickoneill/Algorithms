fe9bc9e6f2f4298e181dd1f05c1e84e9fc584caf
danpatrickoneill <dan47573@gmail.com>
2020-01-22 18:10:47 -0500

    Cache initialization; tightened up code

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -14,21 +14,23 @@ import time
 
 
 def eating_cookies(n,  cache=None):
+    # Initialize cache as needed
+    if cache is None:
+        cache = [0 for i in range(n+1)]
+
     if n <= 1:
         return 1
     if n == 2:
         return 2
-    if cache:
-        # Check cache at index n
-        if cache[n]:
-            return cache[n]
-        # If it doesn't exist, cache return value
-        else:
-            cache[n] = eating_cookies(
-                n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    # Check cache at index n
+    if cache[n]:
+        return cache[n]
+    # If it doesn't exist, cache return value
+    else:
+        cache[n] = eating_cookies(
+            n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
     # print(cache)
-    return eating_cookies(
-        n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    return cache[n]
 
 # Small inputs only version
 # def eating_cookies(n,  cache=None):
