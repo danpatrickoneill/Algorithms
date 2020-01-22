bb5d0db1565a2c9d2b0fd5765f72f282d9deeed8
danpatrickoneill <dan47573@gmail.com>
2020-01-22 18:38:51 -0500

    Notes on potentially different if statement order

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import argparse
 import math
 
@@ -9,7 +8,10 @@ def find_max_profit(prices):
     profit = prices[1] - prices[0]
     for price in prices[1:]:
         # if a lower price is found, assign it to buying_price
+        # could also move this below profit check, which eliminates need for continue
         if price < buying_price:
+            if (price - buying_price) > profit:
+                profit = price - buying_price
             buying_price = price
             # if price has been updated this round, don't need to check profit
             continue
@@ -19,7 +21,10 @@ def find_max_profit(prices):
     return profit
 
 
-print(find_max_profit([100, 90, 80, 50, 20, 10]))
+if __name__ == '__main__':
+    print(find_max_profit([10, 7, 5, 8, 11, 9]))
+    print(find_max_profit([100, 90, 80, 50, 20, 15]))
+
 
 if __name__ == '__main__':
     # This is just some code to accept inputs from the command line
